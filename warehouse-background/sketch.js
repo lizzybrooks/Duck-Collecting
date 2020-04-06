cd8ad8bcde84666e7bae46ddd4db1c052f7a4beb
Kate Peterson <61802919+kitteap@users.noreply.github.com>
2020-04-06 13:22:59 -0700

    added some background

--- warehouse-background/sketch.js
+++ warehouse-background/sketch.js
@@ -5,11 +5,15 @@ let squares = [];
 
 
 function setup() {
-  createCanvas(1000, 500);
+  createCanvas(1200, 600);
 
 }
 
 function draw(){
-	background(255,255,255);
+	background(128,128,128);
+  fill(178,178,178);
+  stroke(115,115,115);
+  strokeWeight(6);
+  rect(0,400,1200,200);
 
 }
