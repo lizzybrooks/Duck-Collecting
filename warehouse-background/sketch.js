54e069fa9e842abb3429cc1bef76e53f90560fa1
Kate Peterson <61802919+kitteap@users.noreply.github.com>
2020-04-06 13:43:35 -0700

    added windows and started first crate

--- warehouse-background/sketch.js
+++ warehouse-background/sketch.js
@@ -5,7 +5,7 @@ let squares = [];
 
 
 function setup() {
-  createCanvas(1200, 600);
+  createCanvas(800, 400);
 
 }
 
@@ -14,6 +14,15 @@ function draw(){
   fill(178,178,178);
   stroke(115,115,115);
   strokeWeight(6);
-  rect(0,400,1200,200);
+  rect(0,264,800,136);
 
+  fill(77,166,255);
+  strokeWeight(10); //windows
+  rect(50,25,300,75);
+  rect(450,25,300,75);
+
+  stroke(96,64,32);
+  strokeWeight(3);
+  fill(220,160,102);
+  rect(85,200,100,100);
 }
